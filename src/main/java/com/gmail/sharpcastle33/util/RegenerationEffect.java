47e5b4dfea83da9ddaca52514183fc49daac37f6
JG <33494600+JJ-GG@users.noreply.github.com>
2018-02-04 16:52:14 -0500

    Added RegenerationEffect.java

---
+++ src/main/java/com/gmail/sharpcastle33/util/RegenerationEffect.java
@@ -0,0 +1,76 @@
+
+
+package com.gmail.sharpcastle33.util;
+
+import java.util.ArrayList;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.entity.Player;
+
+
+
+public RegenerationEffect extends BukkitRunnable {
+
+
+Player player;
+double regenLevel;
+ArrayList<RegenerationEffect> regenList;
+int ticks;
+double regenAmount = 1;
+
+public RegenerationEffect(Player player, double initialRegenLevel, ArrayList regenList){
+
+  this.player = player; // The player who is receiving regen
+  this.regenLevel = initialRegenLevel; // When this object is created, the initial amount of regen the player recieves
+  this.regenList = regenList; // List of players who are receiving regen (used to take player off if regenLevel = 0);
+  this.ticks = 0; // Time passed
+  
+  
+
+}
+
+@Override
+  public void run(){
+  
+    ticks++;
+    
+    if(regenLevel == 0){
+        regenList.remove(this);
+        this.cancel();
+    }
+    
+    
+    if(regenLevel < 4){
+      regenAmount = 1; // .5 Heart
+    } else
+    if(regenLevel < 8){
+      regenAmount = 2; // 1 Heart
+    } else
+    if(regenLevel < 12){
+      regenAmount = 3; // 1.5 Hearts
+    } else
+    if(regenLevel == 12){
+      regenAmount = 4; // 2 Hearts
+    }
+    
+    
+    if(ticks % 300 == 0){ // procs every 15 seconds
+    
+        if(player.getMaxHealth() != player.getHealth()){
+            player.setHealth(player.getHealth() + regenAmount);
+        }
+        
+    }
+
+  }
+  
+  
+  public void removeLevels(int amount){
+      regenLevel -= amount;
+  }
+  
+  public void addLevels(int amount){
+      regenLevel += amount;
+  }
+  
+
+}
