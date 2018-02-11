075f00e2e704300de3b3970ccf37e1d67c72b798
JG <33494600+JJ-GG@users.noreply.github.com>
2018-02-10 21:14:04 -0500

    Created RageEffect.java

---
+++ src/main/java/com/gmail/sharpcastle33/util/RageEffect.java
@@ -0,0 +1,53 @@
+package com.gmail.sharpcastle33.util;
+
+import java.util.ArrayList;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.entity.Player;
+
+
+
+public RageEffect extends BukkitRunnable {
+
+  int level;
+  int duration;
+  Player player;
+
+public RageEffect(Player player){
+
+  level = 1;
+  duration = 100; // 5 seconds
+  this.player = player;
+  
+  this.runTask(CivEnchant.plugin);
+
+}
+
+@Override
+  public void run(){
+  
+    duration--;
+    
+    if(duration <= 0){
+    
+        int playerIndex = CivEnchant.cdManager.ragePlayers.indexOf(player);
+				
+				CivEnchant.cdManager.ragePlayers.remove(playerIndex);
+				CivEnchant.cdManager.rageEffects.remove(playerIndex);
+        this.cancel();
+     }
+  }
+  
+  public int getLevel(){
+    return level;
+  }
+  
+  public void incrementLevel(){
+      level++;
+      duration = level * 20;
+  }
+    
+    
+    
+    
+
+}
