ae3127fa16309dda82ecd803b18c01ba061350a6
KingVictoria <TheRealKingVictoria@gmail.com>
2018-09-04 10:23:52 -0400

    removed hidden gitignore--must've been created with a misclick

---
+++ src/main/java/com/gmail/sharpcastle33/debugtools/ListCivEnchantsCommand.java
@@ -0,0 +1,32 @@
+package com.gmail.sharpcastle33.debugtools;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import com.gmail.sharpcastle33.enchantments.CustomEnchantment;
+
+public class ListCivEnchantsCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(!sender.isOp()) {
+			sender.sendMessage(ChatColor.RED + "You do not have permission to use this command");
+			return true;
+		} // if
+		
+		String out = "";
+		
+		for(CustomEnchantment ce: CustomEnchantment.values()) {
+			out += ce.toString() + ", ";
+		}
+		
+		out = out.substring(0, out.length() - 2);
+		
+		sender.sendMessage(ChatColor.AQUA + out);
+		
+		return true;
+	} // onCommand
+
+} // class
