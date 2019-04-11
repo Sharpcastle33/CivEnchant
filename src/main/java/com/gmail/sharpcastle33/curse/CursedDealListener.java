876b44e2e90fd4faa76b8e5ed3adc5ff37d891b1
Sharpcastle33 <sharpcastle33@gmail.com>
2019-04-11 13:10:32 -0400

    transaction logic

--- src/main/java/com/gmail/sharpcastle33/curse/CursedDealListener.java
+++ src/main/java/com/gmail/sharpcastle33/curse/CursedDealListener.java
@@ -1,6 +1,7 @@
 package com.gmail.sharpcastle33.curse;
 
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,15 +9,17 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import net.md_5.bungee.api.ChatColor;
 
 public class CursedDealListener implements Listener{
   
+  public static String ERROR_NOT_ENOUGH_COINS = ChatColor.RED + "You do not have enough Cursed Coins to purchase this item!";
+  public static String DEAL_SUCCESS = "DEAL_SUCCESS";
   @EventHandler
   public void triggerDeal(PlayerInteractEvent event) {
     if(event.getHand() == EquipmentSlot.HAND && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)){
       Player p = event.getPlayer();
       ItemStack main = p.getInventory().getItemInMainHand();
-      ItemStack off = p.getInventory().getItemInOffHand();
         
       if(main == null) { return; }
         
@@ -27,6 +30,20 @@ public class CursedDealListener implements Listener{
         ItemStack deal = p.getInventory().getItemInMainHand();
         
         int coins = CurseUtil.parseCoins(deal);
+        
+        //if the player has enough coins
+        if(CurseUtil.hasEnoughCoins(p, coins)) {
+          
+          //Complete transaction
+          CurseUtil.removeCoins(p, coins);
+          p.getInventory().setItemInMainHand(CurseUtil.parseItem(deal));
+          p.sendMessage(DEAL_SUCCESS);
+          p.getWorld().playSound(p.getLocation(), Sound.AMBIENT_CAVE, 1, 1);
+
+          
+        }else {
+          p.sendMessage(ERROR_NOT_ENOUGH_COINS);
+        }
         
       }
     }
